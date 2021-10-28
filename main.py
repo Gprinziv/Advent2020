33128f091b362a8f537c75408ba1591cb65bbc21
Gprinziv <gprinziv@gmail.com>
2021-10-28 14:52:11 +0900

    Progress on the part 2

--- main.py
+++ main.py
@@ -1,56 +1,64 @@
 from os import name
+def part1():
+  with open("tiletest") as file:
+    tiles = [x.split("\n") for x in file.read().split("\n\n")]
+    for i in range(len(tiles)):
+      tiles[i] = [int(tiles[i][0].split(" ")[1][:-1]), tiles[i][1:]]
 
-with open("tiletest") as file:
-  tiles = [x.split("\n") for x in file.read().split("\n\n")]
+  edgeDict = {}
+  matches = {}
+  for tile in tiles:
+    left =  "".join(tile[1][i][0] for i in range(len(tile[1])))
+    right = "".join(tile[1][i][-1] for i in range(len(tile[1])))
+    edgeDict[tile[0]] = [tile[1][0], tile[1][-1], left, right, tile[1][0][::-1], tile[1][-1][::-1], left[::-1], right[::-1]]
+    matches[tile[0]] = []
+
   for i in range(len(tiles)):
-    tiles[i] = [int(tiles[i][0].split(" ")[1][:-1]), tiles[i][1:]]
+    for j in range(i + 1, len(tiles)):
+      if set(edgeDict[tiles[i][0]]) & set(edgeDict[tiles[j][0]]):
+        matches[tiles[i][0]].append(tiles[j][0])
+        matches[tiles[j][0]].append(tiles[i][0])
 
-edgeDict = {}
-matches = {}
-for tile in tiles:
-  left =  "".join(tile[1][i][0] for i in range(len(tile[1])))
-  right = "".join(tile[1][i][-1] for i in range(len(tile[1])))
-  edgeDict[tile[0]] = [tile[1][0], tile[1][-1], left, right, tile[1][0][::-1], tile[1][-1][::-1], left[::-1], right[::-1]]
-  matches[tile[0]] = []
+  total = 1
+  for tile, match in matches.items():
+    if len(match) == 2:
+      #print(tile)
+      total *= tile
+  print(total)
 
-for i in range(len(tiles)):
-  for j in range(i + 1, len(tiles)):
-    if set(edgeDict[tiles[i][0]]) & set(edgeDict[tiles[j][0]]):
-      matches[tiles[i][0]].append(tiles[j][0])
-      matches[tiles[j][0]].append(tiles[i][0])
-
-total = 1
-for tile, match in matches.items():
-  if len(match) == 2:
-    print(tile)
-    total *= tile
-print(total)
+  return edgeDict, matches
 
 #Choose one of the tiles that only has two matches and designate it (0, 0). Choose one of the matches and designate it (0,1).
 #Find the side of (0,1) that matches the (0,0), orient it, and repeat, finsing the side of (0,2). Repeat until you get to the "corner".
 #Then find the other match for the corner, (1, n), and work backwards to (1,0). Then (2, 0) to (2, n) and repeat. I could clean out the "matches" map as I go to save time.
 
-#edgeDict orientation: N, S, W, E, RN, RS, RW, RE
+
 
 #Get the first corner to kick things off.
 def part2(edgeDict, matches):
-  for key, val in matches.items():
-    if len(val) == 2:
-      next = key
-      nextMatch = val[0]
-      break
+  facings = {0:(), 1:(), 2:(), 3:() ,4:() ,5:() , 6:(), 7:()}
+
+
   tilemap = [[]]
-  print(next)
-  print(nextMatch)
-  matchEdge = edgeDict[next].index(list(set(edgeDict[next]) & set(edgeDict[nextMatch]))[0])
+  next, nextMatch = getCorner(matches)
+  print("First corner is: " + str(next))        ##Print test
+  print("(0,1) is: " + str(nextMatch))          ##Print test
+
+  
+  matchEdge = sorted(list(set(edgeDict[next]) & set(edgeDict[nextMatch])))[0]
+  print(edgeDict[next].index(matchEdge))
+  print(edgeDict[nextMatch].index(matchEdge))
 
 
 
-  tilemap[0][0] = [next, ]
 
+  #edgeDict orientation: N, S, W, E, RN, RS, RW, RE
+
+  #Object tile: [tilenumber, rotation (90 degrees clockwise), reversed]
+  tilemap[0].append([next, False, 0])
+  print(tilemap)
 
   while matches:
-    matchEdge = list(set(edgeDict[next]) & set(edgeDict[nextMatch]))
     #Both the reversed and non-reverse will match. Better to take the non-reverse and only flip the entire row if we find that we need to on the next go.
     #We know that once we get the first edge of row 2, the orientation will be locked.
     print (matchEdge)
@@ -68,6 +76,14 @@ def part2(edgeDict, matches):
 #Store it in a 2d array of tuples? 
 
 
+#Get the first corner to start this whole shebang off.
+def getCorner(matches):
+  for key, val in matches.items():
+    if len(val) == 2:
+      corner = key
+      nextMatch = val[0]
+      return corner, nextMatch
+
 #Figure out what the next tile is sequentially based on the position of the match. Be sure to store what modifications the tiles need 
 def getNext():
   return None
@@ -76,8 +92,8 @@ def getNext():
 def makeMap():
   return None
 
-#Fetches a tile to be added to the map.
-def getTile(number, reversed = False, rotate = 0):
+#Adds a tile to the map.
+def addTile(number, matchEdge, targetDirection):
   return None
 
 #Rotate a tile 90 degrees clockwise or counterclockwise. Should also be able to rotate the map in the final go.
@@ -87,3 +103,7 @@ def rotate():
 #Reverse a tile on the horizontal or vertical axis. Should also be able to reverse the map in the final go.
 def reverse():
   return None
+
+
+edgeDict, matches = part1()
+part2(edgeDict, matches)
